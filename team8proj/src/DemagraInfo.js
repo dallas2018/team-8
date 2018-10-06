bf785c992b2ca3e51a373f923d3328904a0246de
utlathia <utlathia@umich.edu>
2018-10-06 06:41:12 -0400

    progress bar

--- team8proj/src/DemagraInfo.js
+++ team8proj/src/DemagraInfo.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
-import './styling.css';
+import React, { Component } from "react";
+
+import { Progress } from "reactstrap";
 
 class DemagraInfo extends Component {
+  constructor(props) {
+    super(props);
+    //this.home = this.home.bind(this);
+    this.state = {
+      gender: this.props.gender,
+      race: this.props.race,
+      vet: this.props.vet,
+      pris: this.props.pris
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.home = this.home.bind(this);
-        this.state = {
-            gender:this.props.gender,
-            race:this.props.race,
-            vet:this.props.vet,
-            pris:this.props.pris,
-        }
-    }
-
-    render() {
-        return (
-            <div>
-              <h1>
-              Please enter your demagraphic information below:
-              </h1>
-                <form>
-                    <label>
-                        Gender:
-                        <input type="text" gender="gender"/>
-                        <div>Race:
-                        <select race = "Race">
-                        <option value="White"></option>
-                        </div>
-                        <div>Veteran Status:
-                        <input type="vet" vet="vet"/>
-                        </div>
-                        <div>Conviction:
-                        <input type="text" pris="pris"/>
-                        </div>
-                    </label>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="text-center">25%</div>
+        <Progress value="25" />
+      </div>
+      // <div>
+      //   <h1>
+      //   Please enter your demagraphic information below:
+      //   </h1>
+      //     <form>
+      //         <label>
+      //             Gender:
+      //             <input type="text" gender="gender"/>
+      //             <div>Race:
+      //             <select race = "Race">
+      //             <option value="White"></option>
+      //             </div>
+      //             <div>Veteran Status:
+      //             <input type="vet" vet="vet"/>
+      //             </div>
+      //             <div>Conviction:
+      //             <input type="text" pris="pris"/>
+      //             </div>
+      //         </label>
+      //     </form>
+      // </div>
+    );
+  }
 }
 
 export default DemagraInfo;
