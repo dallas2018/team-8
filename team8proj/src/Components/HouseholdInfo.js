cb77e7132b3239299b3e976ab002c3725d01ee92
Joel Jijo <JoelJ@Joels-MacBook-Pro.local>
2018-10-06 06:09:09 -0500

    household

--- team8proj/src/Components/HouseholdInfo.js
+++ team8proj/src/Components/HouseholdInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText,Col, Row } from 'reactstrap';
 
-export default class Example extends React.Component {
+export default class ContactInfo extends React.Component {
   render() {
     return (
       <Row>
