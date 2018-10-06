fe8b294ccb37c6ed38458acfff91bf41e2419da8
Justin Nguyen <jvnguyen30@yahoo.com>
2018-10-06 00:49:05 -0500

    branch

--- team8proj/src/HouseholdInfo.js
+++ team8proj/src/HouseholdInfo.js
@@ -4,7 +4,18 @@ class HouseholdInfo extends Component{
     constructor(props)
     {
         super(props)
-        this.state = {maritalStatus: this.props.maritalStatus}
+        this.state = 
+        {
+            maritalStatus: this.props.maritalStatus,
+            annualIncome: this.props.annualIncome,
+            numOfChildren: this.props.numOfChildren,
+            numOfYoungAdults: this.props.numOfYoungAdults,
+            numOfAdults: this.props.numOfAdults,
+            checkingAccount: this.props.checkingAccount,
+            savingsAccount: this.props.savingsAccount,
+            paydayLoan: this.props.paydayLoan,
+            carTitleLoan: this.props.carTitleLoan
+        }
     }
 
     render()
@@ -14,6 +25,37 @@ class HouseholdInfo extends Component{
                 {this.state.maritalStatus}
                 {console.log(this.props.maritalStatus)}
             </div>
+            <form>
+                <label>
+                        Marital Status:  
+                        <input type="text" name="name"/>
+                        <div>Email:  
+                        <input type="text" email="email"/>
+                        </div>
+                        <div>Password:  
+                        <input type="text" password="password"/>
+                        </div>
+                        <div>Street: 
+                        <input type="text" street="street"/>
+                        </div>
+                        <div>City: 
+                        <input type="text" city="city"/>
+                        </div>
+                        <div>State: 
+                        <input type="text" state="state"/>
+                        </div>
+                        <div>Zip: 
+                        <input type="text" zip="zip"/>
+                        </div>
+                        <div>Phone: 
+                        <input type="text" phone="phone"/>
+                        </div>
+                        <div>DOB: 
+                        <input type="text" dob="DOB"/>
+                        </div>
+                </label>
+            </form>
         )
     }
 }
+export default HouseholdInfo;
